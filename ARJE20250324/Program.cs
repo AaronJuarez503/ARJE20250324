ab0efdd13d98dd069b7f90cfa0ccdce2c98cb39d
Aaron Juarez <Aaron Juarez@DESKTOP-DKTIVHI>
2025-03-24 08:53:32 -0600

    Configuracion del proyecto

--- ARJE20250324/Program.cs
+++ ARJE20250324/Program.cs
@@ -1,8 +1,14 @@
+using ARJE20250324.AppWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<Test20250324DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
