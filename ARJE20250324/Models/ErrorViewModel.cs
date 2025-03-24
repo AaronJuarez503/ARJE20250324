580040b1690ee368e14db2ced5611a9b175234d3
Aaron Juarez <Aaron Juarez@DESKTOP-DKTIVHI>
2025-03-24 08:49:58 -0600

    Agregar archivos de proyecto.

---
+++ ARJE20250324/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ARJE20250324.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ARJE20250324/Models/Test20250324DbContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ARJE20250324.AppWebMVC.Models;
+
+public partial class Test20250324DbContext : DbContext
+{
+    public Test20250324DbContext()
+    {
+    }
+
+    public Test20250324DbContext(DbContextOptions<Test20250324DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Warehouse> Warehouses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Brands__3214EC07A27A0067");
+
+            entity.Property(e => e.BrandName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07E2424334");
+
+            entity.Property(e => e.BrandId).HasColumnName("BrandID");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PurchasePrice).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.WarehouseId).HasColumnName("WarehouseID");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK__Products__BrandI__4E88ABD4");
+
+            entity.HasOne(d => d.Warehouse).WithMany(p => p.Products)
+                .HasForeignKey(d => d.WarehouseId)
+                .HasConstraintName("FK__Products__Wareho__4D94879B");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC0758AB1D34");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D105345960647A").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.Password)
+                .HasMaxLength(32)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Warehouse>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Warehous__3214EC074A374955");
+
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.WarehouseName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
